416da2857ffffa61b2db8d020b740eb60a47dbbc
Jason Dagana <j.dagana@teflonhub.com>
2025-03-20 15:25:38 +0100

    fix the issue'

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/countries/',
+  base: '/',
   plugins: [react()],
    publicDir: "public"
  })
-
-
