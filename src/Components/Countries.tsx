e79a1a2d15917b5c5ea94158c3fd8fe6a54080e1
Jason Dagana <j.dagana@teflonhub.com>
2025-03-22 04:13:04 +0100

    fixed updating state inside render

--- src/Components/Countries.tsx
+++ src/Components/Countries.tsx
@@ -2,21 +2,22 @@ import SearchAndFilter from "./SearchAndFilter";
 import CountryCard from "./CountryCard";
 import { useOutletContext } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { countryReducerAction } from "./store";
 
 export default function Countries() {
     const {theme, resolvedData: loaderData } = useOutletContext<any>()
    const dispatch = useDispatch()
-    const reduxCountry = useSelector<any>((state: { countryData: { data: any; }; }) => state.countryData.data)
-    useEffect(() => {
-        if(!reduxCountry){
-            dispatch(countryReducerAction.getData(loaderData))
-        }
-    }, [reduxCountry, dispatch, loaderData])
-    // if (!loaderData) {
-    //     return <h1 className="h-[100vh] flex justify-center items-center text-green-500 text-3xl text-center"> Loading...</h1>;
-    // }else{
+    const reduxCountry = useSelector<any>((state: { countryData: { data: any; } }) => state.countryData.data)
+    
+const hasDispatched = useRef(false);
+
+useEffect(() => {
+    if (!hasDispatched.current && !reduxCountry) {
+        dispatch(countryReducerAction.getData(loaderData));
+        hasDispatched.current = true; // Prevents multiple dispatches
+    }
+}, [reduxCountry, dispatch, loaderData]);
 
         return (
             <div>
